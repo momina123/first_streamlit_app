8ccb76a0d94ce0155c76e036e21ba305c482cb49
Momina Nasar <44096468+momina123@users.noreply.github.com>
2023-10-18 12:43:49 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,5 +11,9 @@ streamlit.text('🐔Hard-Boiled Free Range Egg')
 streamlit.text('🥑🍞Avacado Toast')
 
 streamlit.title('Build your own fruit smoothie')
+# Let's put a pick list here so they can pick the fruit they want to include 
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+
 streamlit.dataframe(my_fruit_list)
+
 
