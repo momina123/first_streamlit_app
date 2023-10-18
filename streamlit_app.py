8ae6500c167642af756fe69b3d4338b6d4b87f66
Momina Nasar <44096468+momina123@users.noreply.github.com>
2023-10-18 10:27:59 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit
 import pandas
 
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣Omega 3 and blueberry Oatmeal')
@@ -9,4 +11,3 @@ streamlit.text('🐔Hard-Boiled Free Range Egg')
 streamlit.text('🥑🍞Avacado Toast')
 streamlit.dataframe(my_fruit_list)
 
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
