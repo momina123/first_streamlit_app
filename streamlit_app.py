236eca1e3a0eaaf838dd41688573d285ae88e52c
Momina Nasar <44096468+momina123@users.noreply.github.com>
2023-10-31 15:14:33 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,14 @@
 import streamlit
 import pandas
 
+streamlit.header("Fruityvice Fruit Advice!")
+import requests
+
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
+
+
 
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
